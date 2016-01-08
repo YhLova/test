4f3993d90059d57de466e5c631d923d2353f4f61
yanhua <yhstudy@sina.com>
2016-01-08 14:45:12 +0800

    55555555

--- src/test/T.java
+++ src/test/T.java
@@ -4,13 +4,6 @@ public class T {
 	
 	public static void main(String[] args) {
 		System.out.println("sssssssssssss");
-		System.out.println("分之一");
-		System.out.println("分之一1阿斯顿发斯蒂芬");
-		System.out.println("分之二阿斯蒂芬");
-		System.out.println("分之三1阿斯蒂芬");
-		System.out.println("分之四");
-		System.out.println("分之wu");
-		System.out.println("分之wussssssssss");
 	}
 
 }
