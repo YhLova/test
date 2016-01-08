c6881df152d13678dba6d2322e3eaa042e880400
yanhua <yhstudy@sina.com>
2016-01-08 15:22:36 +0800

    sdf

--- src/test/F.java
+++ src/test/F.java
@@ -3,7 +3,7 @@ package test;
 public class F {
 	
 	public static void main(String[] args) {
-		System.out.println("sfffffffffffffffffff");
+		System.out.println("sffffffffffffffdddddddddddfffff");
 	}
 
 }
